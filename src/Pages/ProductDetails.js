4c1426eb059cbe9ff66471063a78e93854af0efc
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-10-06 21:54:22 +0300

    Created delete function

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,9 +1,34 @@
-function ProductDetails (){
-    return(
-        <>
-        <h1 className="mt-3">ProductDetails</h1>
-        </>
-    );
-}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import './ProductDetails.css'
 
-export default ProductDetails;
+
+function ProductDetails() {
+    const api_url = "https://fakestoreapi.com/products";
+    const { productID } = useParams();
+    const [product, setProduct] = useState({}); 
+  
+    useEffect(() => {
+      fetch(`${api_url}/${productID}`) 
+        .then((response) => response.json())
+        .then((data) => setProduct(data)) 
+        .catch((error) => console.error("Error fetching product:", error)); 
+    }, [productID]);
+  
+    return (
+      <>
+      {product && <div className="product-details">
+      <h1 className="product-title">{product.title}</h1>
+      <img className="product-image" src={product.image} alt={product.title} />
+      <p className="product-price">Price: ${product.price}</p>
+      <p className="product-description">{product.description}</p>
+      <p className="product-category">Category: {product.category}</p>
+      <p className="product-rating">
+        Rating: {product.rating?.rate} / 5 ({product.rating?.count} reviews)
+      </p>
+          </div>}
+      </>
+    );
+  }
+  
+  export default ProductDetails;
